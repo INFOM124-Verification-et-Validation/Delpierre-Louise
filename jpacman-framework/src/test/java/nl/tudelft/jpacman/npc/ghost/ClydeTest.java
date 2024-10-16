63d1c58b5cfd02e5d1cbf85b1c11eb694ae08e73
Louise Delpierre <louise.delpierre@student.unamur.be>
2024-10-16 16:53:43 +0200

    Création Clyde

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -1,4 +1,13 @@
 package nl.tudelft.jpacman.npc.ghost;
 
-public class ClydeTest { 
+import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ClydeTest {
+    PacManSprites SPRITE_STORE = new PacManSprites();
+    GhostFactory factory= new GhostFactory(SPRITE_STORE);
+    Ghost clyde= factory.createClyde();
+
+
 }
