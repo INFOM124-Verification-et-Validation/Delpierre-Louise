05a9c12d4adbee707fef928728d074aeaaefaef0
Louise Delpierre <louise.delpierre@student.unamur.be>
2024-10-23 14:39:47 +0200

    rajout de commentaire

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -2,12 +2,19 @@ package nl.tudelft.jpacman.npc.ghost;
 
 import nl.tudelft.jpacman.npc.Ghost;
 import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class ClydeTest {
+    // Création d'un clyde
     PacManSprites SPRITE_STORE = new PacManSprites();
     GhostFactory factory= new GhostFactory(SPRITE_STORE);
     Ghost clyde= factory.createClyde();
+
+    @BeforeEach
+    
+
+    @Test
 
 
 }
