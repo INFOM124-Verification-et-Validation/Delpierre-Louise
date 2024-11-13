bc4347eb840eb56329eebe9a4bd468da88426d0e
Louise Delpierre <louise.delpierre@student.unamur.be>
2024-11-13 15:43:30 +0100

    Création de 2 tests

--- GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -8,51 +8,45 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
+    public void decreaseQuality(Item item) {
+        if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+            item.quality = item.quality - 1;
+        }
+    }
+    public void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals(BACKSTAGE)) {
-                if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
+            if (!item.name.equals("Aged Brie") && !item.name.equals(BACKSTAGE)) {
+                decreaseQuality(item);
 
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE)) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-
-
-                        if (item.sellIn < 6 && item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-
-                    }
-                }
+               increaseQuality(item);
+               if (item.name.equals(BACKSTAGE)) {
+                   if (item.sellIn < 11 && item.quality < 50) {
+                       item.quality = item.quality + 1;
+                   }
+                   if (item.sellIn < 6 && item.quality < 50) {
+                       item.quality = item.quality + 1;
+                   }
+               }
             }
-
             if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
-
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals(BACKSTAGE)) {
-                        if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-
+                        decreaseQuality(item);
                     } else {
                         item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
             }
         }
--- GildedRose-Refactoring-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,5 +13,39 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
-
+    @Test
+    void neverNegative () {
+        Item[] items = new Item [] {
+            new Item("item1", 10 , 5 ),
+            new Item("item2", 5, 0),
+            new Item("item3", 0, 3),
+            new Item("item4", -1, 1)
+        };
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 20; i++) {
+            app.updateQuality();
+            for (Item item : items) {
+                assertEquals(true, item.quality >= 0, "Quality should never be negative");
+            }
+        }
+    }
+    @Test
+    void cinquainQuality() {
+        Item[] items = new Item [] {
+            new Item("item1", 10 , 11 ),
+            new Item("item2", 5, 0),
+            new Item("item3", 0, 43),
+            new Item("item4", -1, 50)
+        };
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < 20; i++) {
+            app.updateQuality();
+            for (Item item : items) {
+                assertEquals(true, item.quality <= 50, "Quality should never be more than 50");
+            }
+        }
+    }
 }
+
+
+
